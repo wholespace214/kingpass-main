dc7efb4e40c05999311395cf461ec7bc368dcc1e
wholespace214 <wholespace.214@gmail.com>
2022-12-23 05:59:46 +0900

    Social Link is connected

--- src/config/images.ts
+++ src/config/images.ts
@@ -1,5 +1,9 @@
 import KingPassLogo from '../assets/svg/kingpass-logo.svg';
 import KingpassCard from '../assets/png/Card.png';
 import KingLogo from '../assets/svg/kinglogo.svg';
+import KingFloki from '../assets/svg/kingfloki.svg';
+import KingLand from '../assets/svg/kingland.svg';
+import KingPad from '../assets/svg/kingpad.svg';
+import FooterLogo from '../assets/svg/footerlogo.svg';
 
-export { KingPassLogo, KingpassCard, KingLogo };
+export { KingPassLogo, KingpassCard, KingLogo, KingFloki, KingLand, KingPad, FooterLogo };
--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -1,6 +1,97 @@
+import { FooterLogo } from 'src/config/images';
 import styled from 'styled-components';
-import { Text } from '../components/text/text';
+import { FaTelegramPlane, FaTwitter, FaYoutube, FaGithub } from 'react-icons/fa';
+import { BsMedium } from 'react-icons/bs';
 
 export const Footer = () => {
-  return <></>;
+  return (
+    <FooterContainer>
+      <Img src={FooterLogo} alt="footer-logo" />
+      <FooterLinks>
+        <FooterLink>Cookie preferences</FooterLink> | <FooterLink>Privacy police</FooterLink> |
+        <FooterLink>Terms of use</FooterLink>
+      </FooterLinks>
+      <ExternalLinks>
+        <a href={'https://t.me/KlNGfinance'} rel="noopener noreferrer">
+          <Icon>
+            <FaTelegramPlane style={{ width: '100%', height: 'auto' }} />
+          </Icon>
+        </a>
+        <a href={'https://twitter.com/kingfinance'} rel="noopener noreferrer">
+          <Icon>
+            <FaTwitter style={{ width: '100%', height: 'auto' }} />
+          </Icon>
+        </a>
+        <a href={'https://www.youtube.com/@kingfinanceco'} rel="noopener noreferrer">
+          <Icon>
+            <FaYoutube style={{ width: '100%', height: 'auto' }} />
+          </Icon>
+        </a>
+        <a rel="noopener noreferrer">
+          <Icon>
+            <FaGithub style={{ width: '100%', height: 'auto' }} />
+          </Icon>
+        </a>
+        <a href={'https://medium.com/@kingfinance'} rel="noopener noreferrer">
+          <Icon>
+            <BsMedium style={{ width: '100%', height: 'auto' }} />
+          </Icon>
+        </a>
+      </ExternalLinks>
+    </FooterContainer>
+  );
 };
+
+const FooterContainer = styled.div`
+  width: 100%;
+  height: 250px;
+  padding-top: 50px;
+  display: flex;
+  font-size: 16px;
+  align-items: center;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 44px;
+  @media screen and (max-width: 540px) {
+    height: 180px;
+    gap: 28px;
+  }
+`;
+
+const Img = styled.img`
+  width: 80px;
+  height: auto;
+  @media screen and (max-width: 540px) {
+    width: 60px;
+  }
+`;
+
+const FooterLinks = styled.div`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  font-size: 16px;
+  @media screen and (max-width: 540px) {
+    font-size: 10px;
+    gap: 10px;
+  }
+`;
+const FooterLink = styled.div``;
+
+const ExternalLinks = styled.div`
+  display: flex;
+  gap: 60px;
+  align-items: center;
+  @media screen and (max-width: 540px) {
+    gap: 40px;
+  }
+`;
+
+const Icon = styled.div`
+  width: 30px;
+  height: 30px;
+  @media screen and (max-width: 540px) {
+    width: 20px;
+    height: 20px;
+  }
+`;
